8a0f6882411a5e41395cb9a7cffe37afe34512e4
carlosdf88 <carlos.df@hotmail.com>
2013-10-18 16:40:10 +0200

    a

---
+++ Unidad_1/src/Ejercicios/ejercicio5.java
@@ -0,0 +1,30 @@
+package Ejercicios;
+
+import java.util.Scanner;
+
+public class ejercicio5 {
+
+	public static void main(String[] args) {
+		
+		Scanner teclado=new Scanner(System.in);
+		double peso,velocidad,seg;
+		
+		System.out.println ("Crea un programa que pida el tamaño de un archivo (Mbytes) y la velocidad de una línea de datos (Mbps), y muestre el tiempo que tardará en transmitirlo usando dicha línea.");
+		System.out.println ("Introduce cuanto ocupa el archivo en Mb: ");
+		peso= teclado.nextDouble();
+
+		System.out.println ("Introduce que velocidad tiene tu conexion en Mbps: ");
+		velocidad= teclado.nextDouble();
+
+		
+		seg=(peso/velocidad)/60;
+				
+		System.out.println("El archivo de: "+peso+" Mb tardará: "+velocidad+" min en enviarse.");
+		
+		
+		
+		
+
+	}
+
+}
