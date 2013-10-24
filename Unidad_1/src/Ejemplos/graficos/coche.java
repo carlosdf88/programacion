e2f075500881843d0485b4c7ffdfa15ae9608d1c
carlosdf88 <carlos.df@hotmail.com>
2013-10-25 01:23:41 +0200

    Ejercicio coche

---
+++ Unidad_1/src/Ejemplos/graficos/coche.java
@@ -0,0 +1,35 @@
+package Ejemplos.graficos;
+
+
+import graphics.Canvas;
+import graphics.Rectangle;
+import graphics.Color;
+import graphics.Line;
+import graphics.Text;
+
+public class coche {
+
+	public static void main(String[] args) {
+		
+		Canvas lienzo;
+		lienzo= Canvas.getInstance();
+		Rectangle rectangulo;
+		
+		rectangulo=new Rectangle(100,100,100,100);
+		rectangulo.draw();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
