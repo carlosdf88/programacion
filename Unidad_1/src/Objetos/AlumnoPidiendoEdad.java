82b6c8d694dea14a459ed8e5444dddd3160db570
carlosdf88 <carlos.df@hotmail.com>
2013-10-24 16:25:12 +0200

    pues eso

--- Unidad_1/src/Objetos/AlumnoPidiendoEdad.java
+++ Unidad_1/src/Objetos/AlumnoPidiendoEdad.java
@@ -7,7 +7,7 @@ public class AlumnoPidiendoEdad {
 	public static void main(String[] args) {
 		
 		Scanner input=new Scanner (System.in);
-		int contador=0,x=0,edad=0;
+		int contador=0,x=0,edad=0,totaledad=0;
 		String nombre="",total="";
 		
 		System.out.println("¿Cuantos alumnos vas a introducir? ");
@@ -24,17 +24,21 @@ public class AlumnoPidiendoEdad {
 				
 				if (x==0){
 				total="\n"+nombre+" tiene "+edad+" años.";
+				
 				}
 	
 				if (x!=0){
 				total=total+"\n"+nombre+" tiene "+edad+" años.";
+				
 				}
+				
 				x++;
+				totaledad=totaledad+edad;
 		}
 
 		
 		System.out.println("\nListado de alumnos"+total);
-		
+		System.out.println("\nLa media de edad de los alumnos es: "+(totaledad/contador)+" años.");
 
 	}
 
