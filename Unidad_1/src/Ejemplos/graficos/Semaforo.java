2a3d02d7afe62ed59310e10eaa809b339d7e25e5
carlosdf88 <carlos.df@hotmail.com>
2013-10-24 17:58:09 +0200

    Semaforo y demas ejemplos

---
+++ Unidad_1/src/Ejemplos/graficos/Semaforo.java
@@ -0,0 +1,103 @@
+package Ejemplos.graficos;
+
+import graphics.Canvas;
+import graphics.Ellipse;
+import graphics.Rectangle;
+import graphics.Color;
+
+
+public class Semaforo {
+
+	public static void main(String[] args) throws InterruptedException {
+	
+		Canvas lienzo;
+		Rectangle rectangulo,rectangulo2,rectangulo3,rectangulo4;
+		Ellipse rojo,ambar,verde,otro,otro2,otro3;
+		lienzo= Canvas.getInstance();
+		Color gris;	
+		gris=new Color(200, 200, 200); 
+	
+		
+		rectangulo = new Rectangle(100,100,110,200);
+		rectangulo.draw();
+		rectangulo2=new Rectangle(100,100,110,200);
+		rectangulo2.draw();
+		rectangulo3=new Rectangle(150,300,20,200);
+		rectangulo3.draw();
+		rectangulo4=new Rectangle(150,300,20,200);
+		rectangulo4.draw();
+		
+				otro=new Ellipse(130,110,50,50);
+					otro.draw();
+		
+				otro2=new Ellipse(130,170,50,50);
+					otro.draw();
+		
+				otro3=new Ellipse(130,230,50,50);
+					otro.draw();
+			
+		rojo=new Ellipse(130,110,50,50);
+		rojo.draw();
+		
+		ambar=new Ellipse(130,170,50,50);
+		ambar.draw();
+		
+		verde=new Ellipse(130,230,50,50);
+		verde.draw();
+		
+		rectangulo.setColor(gris);	
+		rectangulo.fill();		
+		rectangulo3.setColor(gris);	
+		rectangulo3.fill();	
+
+	
+		otro.setColor(Color.BLACK);
+		
+		otro2.setColor(Color.BLACK);
+		
+		otro3.setColor(Color.BLACK);
+
+		
+	
+		rojo.setColor(Color.RED);
+		rojo.fill();
+		
+		Thread.sleep(3000);
+		
+		ambar.setColor(Color.YELLOW);
+		ambar.fill();
+		
+		otro.setColor(Color.BLACK);
+	
+		otro2.setColor(Color.BLACK);
+		
+		otro3.setColor(Color.BLACK);
+
+		
+		rojo.setColor(gris);
+		rojo.fill();
+		
+		
+		Thread.sleep(5000);
+		
+		otro.setColor(Color.BLACK);
+		
+		otro2.setColor(Color.BLACK);
+		
+		otro3.setColor(Color.BLACK);
+
+		
+		
+		verde.setColor(Color.GREEN);
+		verde.fill();
+	
+		ambar.setColor(gris);
+		ambar.fill();
+		
+		rojo.setColor(gris);
+		rojo.fill();
+	
+	
+	}
+
+}
---
+++ Unidad_1/src/Ejemplos/graficos/graficos1.java
@@ -0,0 +1,36 @@
+package Ejemplos.graficos;
+
+import graphics.Canvas;
+import graphics.Rectangle;
+import graphics.Color;
+
+public class graficos1 {
+
+	public static void main(String[] args) {
+		
+		Canvas lienzo;
+		Rectangle rectangulo;
+		
+		lienzo= Canvas.getInstance();			//creamos un lienzo "sitio en el que trabajar", con esto creamos una 
+												//ventana en el que apareceran todas las movidas que vayamos haciendo.
+		
+				
+				
+		rectangulo = new Rectangle(100,100,100,50); //Aki ponemos como queremos el rectangulo, las dos primeras cifras
+												  // indican cuanto queremos que se aleje de los bordes (x,y) y las dos
+												  // siguientes indican el tamaño del rectangulo.
+				
+		rectangulo.draw();						//con este comando dibujamos el objeto que hemos creado.
+		Canvas.pause();						  // en este paso se pausa el programa y te sale una ventanita. Hasta que no le des a aceptar no continua.
+		rectangulo.translate(250,0);  		// en este paso mueve el rectangulo de sitio.
+		
+		rectangulo.setColor(Color.ORANGE);	//Con esto aplicamos el color naranja, esto solo colorea el borde.
+		rectangulo.fill();					//Con esto coloreamos el objeto dibujado
+		
+		Color gris;							//Con esto creamos un color nuevo de tipo Color.
+		gris=new Color(200, 200, 200);     //Aqui Modificamos el color gris creado que esta vacio y le asignamos los valores del color gris.
+		Canvas.pause();	
+		rectangulo.setColor(gris);			//Aqui coloreamos del color gris.
+	}
+
+}
